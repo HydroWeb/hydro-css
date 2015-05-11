317007a8524a0bec4ad5bb218381b253b4c0fc1e
Benjamin Fleming <benjfleming21@gmail.com>
2015-05-11 20:16:23 +1000

    Added SCSS linter and grunt options for demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,19 @@ module.exports = function(grunt)
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
+		scsslint: {
+
+			lint: {
+				allFiles: ['./scss/*.scss'],
+				options: {
+					config: '.scss-lint.yml',
+					reporterOutput: 'scss-lint-reporter.xml'
+				}
+			}
+		},
+
 		sass: {
+
 			test: {
 				options: {
 					style: 'expanded',
@@ -16,20 +28,47 @@ module.exports = function(grunt)
 				files: {
 					'./spec/results.css' : './spec/tests.scss'
 				}
+			},
+
+			demo: {
+				options: {
+					style: 'expanded',
+					sourcemap: 'none',
+					loadPath: ['./scss']
+				},
+				files: {
+					'./demo/styles/app.css' : './demo/styles/src/app.scss'
+				}
 			}
 		},
 
 		bootcamp: {
+
 			test: {
 				files: {
 					src: ['./spec/results.css']
 				}
 			}
+		},
+
+		autoprefixer: {
+
+			demo: {
+				options: {
+					browsers: ['last 4 versions']
+				},
+				src: './demo/styles/app.css',
+				dest: './demo/styles/app.css'
+			}
 		}
 	});
 
+	grunt.loadNpmTasks('grunt-scss-lint');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('bootcamp');
+	grunt.loadNpmTasks('grunt-autoprefixer');
 
-	grunt.registerTask('test', ['sass', 'bootcamp']);
+	grunt.registerTask('test', ['scsslint:test', 'sass:test', 'bootcamp:test']);
+	grunt.registerTask('lint', ['scsslint:lint']);
+	grunt.registerTask('demo', ['sass:demo', 'autoprefixer:demo']);
 };
